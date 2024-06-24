404661d67430ec01b067d41a186a9086e0ceb939
connerc11 <cccochrane1@gmail.com>
2024-06-24 18:49:20 -0400

    adding to try and fix deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/2024-React-Portfolio",
   plugins: [react()],
   css: {
     modules: {
